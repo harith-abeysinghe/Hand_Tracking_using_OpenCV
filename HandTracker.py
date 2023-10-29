860a03ae3e993c961d5375e5b21621deee34d99c
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-10-29 14:18:50 +0530

    Update HandTracker.py

--- HandTracker.py
+++ HandTracker.py
@@ -17,21 +17,29 @@ class HandDetector():
     def find_hands(self, img, draw=True):
 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
+        self.results = self.hands.process(imgRGB)
         # print(results.multi_hand_landmarks)
 
-        if results.multi_hand_landmarks:
-            for hand_landmark in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for hand_landmark in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, hand_landmark, self.mpHands.HAND_CONNECTIONS)
         return img
 
-    # for id, landmark in enumerate(hand_landmark.landmark):
-    #     # print(id, landmark)
-    #     height, width, channel = img.shape
-    #     cx, cy = int(landmark.x * width), int(landmark.y * height)
-    #     print(id, cx, cy)
 
+    def find_postions(self,img, hand_num = 0, draw = True):
+        landmark_list = []
+        if self.results.multi_hand_landmarks:
+            hand_landmark = self.results.multi_hand_landmarks[hand_num]
+            for id, landmark in enumerate(hand_landmark.landmark):
+                # print(id, landmark)
+                height, width, channel = img.shape
+                cx, cy = int(landmark.x * width), int(landmark.y * height)
+                # print(id, cx, cy)
+                landmark_list.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx,cy), 15, (255,100,100), cv2.FILLED)
+        return  landmark_list
 
 def main():
     previous_time = 0
@@ -44,6 +52,15 @@ def main():
     while 1:
         success, img = webcam.read()
         img = detector.find_hands(img)
+        # landmark_list = detector.find_postions(img)
+
+        # if len(landmark_list)!=0:
+        #     print(landmark_list[5])
+
+        key = cv2.waitKey(1)
+        if key == 27:  # 27 is the ASCII code for 'Esc'
+            break
+
         current_time = time.time()
         fps = 1 / (current_time - previous_time)
         previous_time = current_time
@@ -52,7 +69,9 @@ def main():
 
         cv2.imshow("Camera", img)
         cv2.waitKey(1)
-
+    # Release the webcam and close the OpenCV window
+    webcam.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
